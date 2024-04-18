1278023f197f198c0881e6c10b6de2248bb02ee8
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2024-04-18 12:13:24 +0530

    created the navbar component

---
+++ imdb-next/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavbarItems from "./NavbarItems";
+
+export default function Navbar() {
+  return (
+    <div className="flex gap-6 dark:bg-gray-600 bg-amber-200 p-4 lg:text-lg justify-center">
+      <NavbarItems title="Trending" param="fetchTrending" />
+      <NavbarItems title="Top Rated" param="fetchTopRated" />
+    </div>
+  );
+}
